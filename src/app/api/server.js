a4f3c39e2c667d6150c200fc6f936680c78403cd
vijay <vijays-iprogrammer>
2019-02-07 11:55:41 +0530

    Add required files

--- src/app/api/server.js
+++ src/app/api/server.js
@@ -5,15 +5,17 @@ const express = require('express'),
     mongoose = require('mongoose');
     config = require('./DB');
 
+    const businessRoute = require('./routes/business.route');
     mongoose.Promise = global.Promise;
     mongoose.connect(config.DB, { useNewUrlParser: true }).then(
       () => {console.log('Database is connected') },
       err => { console.log('Can not connect to the database'+ err)}
     );
-    
+
     const app = express();
     app.use(bodyParser.json());
     app.use(cors());
+    app.use('/business', businessRoute);
     let port = process.env.PORT || 4000;
 
     const server = app.listen(function(){
--- src/app/business.service.ts
+++ src/app/business.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class BusinessService {
 
-  uri = 'http://localhost:4200';
+  uri = 'http://localhost:4000';
 
   constructor(private http: HttpClient) { }
 
